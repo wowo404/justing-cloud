5e7b3a5d02bc3222c876332f1a314f3dd5bf9995
刘章盛 <hello-ketty-honey@163.com>
2021-11-23 11:05:43 +0800

    添加common cache模块

---
+++ cloud-common/cloud-common-cache/src/main/java/org/liu/common/cache/RedisCode.java
@@ -0,0 +1,33 @@
+package org.liu.common.cache;
+
+import java.time.Duration;
+
+/**
+ * @date ：Created in 2019/3/1 13:31
+ * @description：${description}
+ * @modified By：
+ * @version: $version$
+ */
+public interface RedisCode {
+    String KAPTCHA = "KAPTCHA_";
+    String CODE = "CODE_";
+    String CODE_DAY_LIMIT = "CODE_DAY_LIMIT_";//格式：key+年月日，存放手机号码的每日发送次数：数据结构：set
+    String TOKEN = "TOKEN_";
+    String CONFIG = "CONFIG_";
+    String SYSTEM_CONFIG = "system_config";
+
+    String LOCK_UPDATE_ORDER_INFO = "lock_update_order_info_";//订单锁，格式：key+订单ID
+    String LOCK_SKU = "lock_sku_";//sku锁，格式：key+skuId
+
+    String WEIXIN_ACCESS_TOKEN = "weixin_access_token";//微信的AccessToken，数据类型：string，保存时间7200
+    String WEIXIN_JSAPI_TICKET = "weixin_jaspi_ticket";//微信的JsAPI_Ticket，数据类型：string，保存时间7200
+	String TODAY_QRCODE_CODE = "today_qrcode_code_";//spu递增编码，每日从1开始递增
+
+    //有效期
+    Duration CODE_EXPIRE_TIME = Duration.ofSeconds(5 * 60L);
+    Duration TOKEN_EXPIRE_TIME = Duration.ofSeconds(7 * 24 * 60 * 60L);
+    Duration LOCK_EXPIRE_TIME = Duration.ofSeconds(6);//锁的失效时间
+    Duration WEIXIN_ACCESS_TOKEN_TIME = Duration.ofSeconds(2 * 60 * 60L);//微信AccessToken失效时间
+    Duration WEIXIN_JSAPI_TICKET_TIME = Duration.ofSeconds(2 * 60 * 60L);//微信JsApiTicket失效时间
+	Long TODAY_CODE_EXPIRE_TIME = Duration.ofDays(1L).getSeconds();//每日递增编码失效时间
+}
---
+++ cloud-common/cloud-common-cache/src/main/java/org/liu/common/cache/RedisHelper.java
@@ -0,0 +1,171 @@
+package org.liu.common.cache;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.scripting.support.StaticScriptSource;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * redis操作帮助类
+ * 因要注入jacksonRedisTemplate，需交给spring做bean管理
+ * 跟util工具区分开来
+ */
+@AllArgsConstructor
+public class RedisHelper {
+
+	private StringRedisTemplate stringRedisTemplate;
+	private RedisTemplate<String, Object> jacksonRedisTemplate;
+
+	/**
+	 * 获取锁
+	 *
+	 * @param key
+	 * @param requestId
+	 * @param expireTime
+	 * @return
+	 */
+	public boolean tryLock(String key, String requestId, Duration expireTime) {
+		return stringRedisTemplate.boundValueOps(key).setIfAbsent(requestId, expireTime);
+	}
+
+	/**
+	 * 释放锁
+	 *
+	 * @param key
+	 * @param requestId
+	 */
+	public boolean releaseLock(String key, String requestId) {
+		String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+		DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>();
+		redisScript.setResultType(Long.class);
+		redisScript.setScriptSource(new StaticScriptSource(script));
+		List<String> keys = Collections.singletonList(key);
+		Long result = stringRedisTemplate.execute(redisScript, keys, requestId);
+		if (null == result || 0 == result) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * 将long类型的id转换为byte，要先转为string，再获取string的bytes
+	 *
+	 * @param id
+	 * @return
+	 */
+	private byte[] getIdField(Long id) {
+		return id.toString().getBytes();
+	}
+
+	/**
+	 * 保存手机验证码
+	 *
+	 * @param mobile
+	 * @return
+	 */
+	public Boolean saveCode(String mobile, String code) {
+		return stringRedisTemplate.boundValueOps(RedisCode.CODE + mobile).setIfAbsent(code, RedisCode.CODE_EXPIRE_TIME);
+	}
+
+	/**
+	 * 获取手机验证码
+	 *
+	 * @param mobile
+	 * @return
+	 */
+	public String getCode(String mobile) {
+		String code = stringRedisTemplate.boundValueOps(RedisCode.CODE + mobile).get();
+		return code;
+	}
+
+	/**
+	 * 删除手机验证码
+	 *
+	 * @param mobile
+	 */
+	public void deleteCode(String mobile) {
+		stringRedisTemplate.delete(RedisCode.CODE + mobile);
+	}
+
+	/**
+	 * 将会话保存至redis
+	 *
+	 * @param token
+	 * @param value
+	 */
+	public void saveToken(String token, Object value) {
+		jacksonRedisTemplate.opsForValue().set(RedisCode.TOKEN + token, value, RedisCode.TOKEN_EXPIRE_TIME);
+	}
+
+	/**
+	 * 删除会话
+	 *
+	 * @param token
+	 */
+	public void deleteToken(String token) {
+		jacksonRedisTemplate.delete(RedisCode.TOKEN + token);
+	}
+
+	/**
+	 * 根据token获取会话
+	 *
+	 * @param token
+	 * @return
+	 */
+	public Object getToken(String token) {
+		return jacksonRedisTemplate.opsForValue().get(RedisCode.TOKEN + token);
+	}
+
+	/**
+	 * 设置token的失效时长
+	 *
+	 * @param token
+	 */
+	public void expireToken(String token) {
+		jacksonRedisTemplate.boundValueOps(RedisCode.TOKEN + token).expire(RedisCode.TOKEN_EXPIRE_TIME.getSeconds(), TimeUnit.SECONDS);
+	}
+
+	public void saveKaptcha(String username, String capText) {
+		jacksonRedisTemplate.boundValueOps(RedisCode.KAPTCHA + username).set(capText);
+	}
+
+	public String getKaptcha(String username) {
+		return (String) jacksonRedisTemplate.boundValueOps(RedisCode.KAPTCHA + username).get();
+	}
+
+	public void deleteKaptcha(String username) {
+		jacksonRedisTemplate.delete(RedisCode.KAPTCHA + username);
+	}
+
+	public String queryTodaySpuCode(String ymd){
+		String s = stringRedisTemplate.boundValueOps(RedisCode.TODAY_QRCODE_CODE + ymd).get();
+		if (null == s) {
+			return "0";
+		}
+		return s;
+	}
+
+	public String getTodayQrcodeCode(String ymd) {
+		return getTodayCode(RedisCode.TODAY_QRCODE_CODE, ymd);
+	}
+
+	private String getTodayCode(String codePrefix, String ymd){
+		//KEY[1]--RedisCode.TODAY_SPU_CODE + ymd
+		//ARGV[1]--RedisCode.TODAY_SPU_CODE_EXPIRE_TIME
+		String script = "if redis.call('exists', KEYS[1]) == 0 then redis.call('set', KEYS[1], 1, 'EX', ARGV[1]); return '1'; " +
+				"else local userCode = redis.call('get', KEYS[1]); redis.call('set', KEYS[1], userCode + 1); return tostring(userCode + 1); " +
+				"end";
+		DefaultRedisScript<String> redisScript = new DefaultRedisScript<>();
+		redisScript.setResultType(String.class);
+		redisScript.setScriptSource(new StaticScriptSource(script));
+		List<String> keys = Collections.singletonList(codePrefix + ymd);
+		String result = stringRedisTemplate.execute(redisScript, keys, RedisCode.TODAY_CODE_EXPIRE_TIME + "");
+		return result;
+	}
+}
