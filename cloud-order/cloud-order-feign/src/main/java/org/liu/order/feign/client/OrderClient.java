cbeb97232a326a098f5aedac2dff564b085bd214
LiuZhangsheng <1879036165@qq.com>
2022-07-01 23:03:51 +0800

    1.移除application.yml，不需要

--- cloud-order/cloud-order-feign/src/main/java/org/liu/order/feign/client/OrderClient.java
+++ cloud-order/cloud-order-feign/src/main/java/org/liu/order/feign/client/OrderClient.java
@@ -1,22 +1,22 @@
 package org.liu.order.feign.client;
 
-import feign.Param;
-import feign.RequestLine;
 import org.justing.commons.model.Response;
 import org.liu.common.core.constants.ServiceNameConstants;
 import org.liu.common.feign.config.FeignConfig;
 import org.liu.order.feign.client.fallback.OrderClientFallback;
 import org.liu.order.feign.pojo.OrderListResp;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @FeignClient(value = ServiceNameConstants.CLOUD_ORDER, fallback = OrderClientFallback.class, configuration = FeignConfig.class)
 //@RequestMapping("order")//千万不要把此注解加上，不然就会出现Ambiguous mapping的错误
-//updated at 20220509，出现此错误的原因很可能是当前service引入了自身的feignClient，现在已不会引入，所有没有这个问题了
 public interface OrderClient {
 
-    @RequestLine("GET /order/queryByUserId/{userId}")
-    Response<List<OrderListResp>> queryByUserId(@Param("userId") Long userId);
+    @GetMapping("/order/queryByUserId/{userId}")
+    Response<List<OrderListResp>> queryByUserId(@PathVariable("userId") Long userId);
 
 }
--- cloud-user/cloud-user-feign/src/main/java/org/liu/user/feign/client/UserClient.java
+++ cloud-user/cloud-user-feign/src/main/java/org/liu/user/feign/client/UserClient.java
@@ -1,7 +1,5 @@
 package org.liu.user.feign.client;
 
-import feign.Param;
-import feign.RequestLine;
 import org.justing.commons.model.Response;
 import org.liu.common.core.constants.ServiceNameConstants;
 import org.liu.common.feign.config.FeignConfig;
@@ -10,18 +8,20 @@ import org.liu.user.feign.pojo.BuyingBehaviorStatisticsReq;
 import org.liu.user.feign.pojo.OperateAccountReq;
 import org.liu.user.feign.pojo.UserResp;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(value = ServiceNameConstants.CLOUD_USER, fallback = UserClientFallback.class, configuration = FeignConfig.class)
 public interface UserClient {
 
-    @RequestLine("GET /user/{userId}")
-    Response<UserResp> getById(@Param("userId") Long userId);
+    @GetMapping("/user/{userId}")
+    Response<UserResp> getById(@PathVariable("userId") Long userId);
 
-    //参数默认就是json格式
-    @RequestLine("POST /user/addBuyingBehaviorStatistics")
-    Response<Void> addBuyingBehaviorStatistics(BuyingBehaviorStatisticsReq req);
+    @PostMapping("/user/addBuyingBehaviorStatistics")
+    Response<Void> addBuyingBehaviorStatistics(@RequestBody BuyingBehaviorStatisticsReq req);
 
-    @RequestLine("POST /user/operateAccount")
-    Response<Void> operateAccount(OperateAccountReq req);
+    @PostMapping("/user/operateAccount")
+    Response<Void> operateAccount(@RequestBody OperateAccountReq req);
 }
